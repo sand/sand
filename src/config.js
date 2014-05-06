97b5fff554fa7c72b88c34d24681bf6444c243a1
upisfree <upisfree@outlook.com>
2014-05-06 20:07:09 +0400

    Base files and the simplest render engine you can imagine

---
+++ src/config.js
@@ -0,0 +1,4 @@
+var config =
+{
+  debug: true	
+};
---
+++ src/engine.js
@@ -0,0 +1,38 @@
+if (config.debug)
+{
+  var stats = new Stats();
+  stats.setMode(0);
+  document.body.appendChild(stats.domElement);
+};
+
+var stage = new PIXI.Stage(0x66FF99);
+
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.view);
+
+requestAnimFrame(animate);
+
+var texture = PIXI.Texture.fromImage('img/ghost.png');
+var bunny = new PIXI.Sprite(texture);
+
+bunny.anchor.x = 0.5;
+bunny.anchor.y = 0.5;
+
+bunny.position.x = 500;
+bunny.position.y = 500;
+
+stage.addChild(bunny);
+
+function animate()
+{
+  if (config.debug)
+    stats.begin();
+
+  requestAnimFrame(animate);
+
+  renderer.render(stage);
+
+  if (config.debug)
+    stats.end();
+}
